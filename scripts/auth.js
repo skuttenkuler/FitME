e77f011fe631accbb00c3af8e3fd5cc9f17e89a6
skuttenkuler <samkiddmusic@gmail.com>
2019-12-15 16:27:45 -0800

    log out, add data to Firestore

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,12 +1,19 @@
 const signUpForm = $(".signup");
 signUpForm.on("submit", function(event){
     event.preventDefault();
-
+    const user = $("#username").val();
+    db.collection(user).add({
+        name: signUpForm.username.value,
+        height:signUpForm.height.value,
+        weight:signUpForm.weight.value,
+        gender:signUpForm.gender.option.value,
+        goal:signUpForm.gainlose.option.value,
+    })
     const email = $("#signupEmail").val();
     const password = $("#signupPassword").val();
 
     auth.createUserWithEmailAndPassword(email,password,username).then(cred => {
-        console.log(cred);
+        //console.log(cred);
    
     });
 });
@@ -21,17 +28,37 @@ loginForm.on("submit", function(event){
     const password = $("#loginPassword").val();
 
     auth.signInWithEmailAndPassword(email, password).then(cred => {
-        console.log(cred.user)
+        //console.log(cred.user)
+        loginForm.reset();
+    });
+});
+//log user out
+const logout = $(".logout");
+logout.on("click", function(event){
+    event.preventDefault();
+    auth.signOut().then(() => {
+        //console.log("signed out")
     })
 })
 
+// listen for state of auth status change
+auth.onAuthStateChanged(user => {
+    //console.log(user)
+    if(user){
+        console.log("user logged in:", user)
+    } else {
+        console.log("user logged out")
+    }
+})
+
 $(".accountbutton").on("click", function(){
-    console.log("clicked");
+    //console.log("clicked");
     $("#signup-modal").attr("style", "display:visible")
     $("#signin-modal").attr("style", "display:none")
 })
 $(".accountbutton2").on("click", function(){
-    console.log("clicked");
+    //console.log("clicked");
     $("#signup-modal").attr("style", "display:none")
     $("#signin-modal").attr("style", "display:visible")
 })
+
